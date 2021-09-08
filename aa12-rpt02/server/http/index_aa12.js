c55cd751430ac35cb91eded9cf6787a84449b49a
성인제 <79964289+nsa32300@users.noreply.github.com>
2021-09-08 17:19:51 +0900

    Add files via upload

---
+++ aa12-rpt02/server/http/index_aa12.js
@@ -0,0 +1,5 @@
+var myinfo = require('./index_module');
+
+myinfo("aa12", "nsa32300", '010-4069-9666');
+
+myinfo("aa55", "Comsi", '010-1234-5678');
---
+++ aa12-rpt02/start/hello_function.js
@@ -0,0 +1,6 @@
+function hello(what) {
+    console.log("Hello " + what + " !");
+}
+
+hello("aa12");
+hello("nsa32300, 성인제");
